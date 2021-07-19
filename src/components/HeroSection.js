e562b0bcd53144eb76d00dc162df757a3362868c
Dibba <roydibba@gmail.com>
2021-07-19 15:17:03 -0400

    Finished landing page with HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import { StockButton } from './StockButton';
+import { Link } from 'react-router-dom';
 
 
 function HeroSection() {
@@ -11,7 +13,10 @@ function HeroSection() {
             <h1>Predict Stock Prices!</h1>
             <p>Use our free tool today!</p>
             <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                <StockButton className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                    STOCK DATA <i class="fas fa-info-circle"></i>
+                </StockButton>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
                     GET STARTED <i class="fas fa-angle-double-right"></i>
                 </Button>
             </div>
