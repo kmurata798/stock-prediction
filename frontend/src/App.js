4dd57245a959864abd0c95b6392664d6134eb355
kmurata798 <kmurata798@gmail.com>
2021-07-19 12:22:50 -0700

    pulling landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:frontend/src/App.js
 import React from 'react';
 import logo from './logo.svg';
-=======
->>>>>>> b1b8e1d4395a62b0e7e4ca4c66099907c447d4d6:src/App.js
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
