1cf5cae20205f9b21adb7e7d5de12a995fc0b12c
Dibba <roydibba@gmail.com>
2021-07-18 22:00:24 -0400

    built navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
-
 
 function Navbar() {
-    const [click,setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        }else{
-            setButton(true);
-        }
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
+  };
 
-    window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    return (
-        <React.Fragment>
-            <nav className="navBar">
-                <div className="navBar-container">
-                    <Link to="/" className="navbar-logo">
-                        Stock Predictor <i className='fab fa-typo3' />
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ?  'fas fa-times' : 'fas fa-bars'} />
+  window.addEventListener('resize', showButton);
 
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/About' className='nav-links' onClick={closeMobileMenu}>
-                                About Us
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>TRY IT!</Button>}
-                </div>
-            </nav>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Stock-Prediction&nbsp;&nbsp;
+            <i class='fas fa-chart-line' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link
+                to='/aboutus'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>USE TOOL</Button>}
+        </div>
+      </nav>
+    </React.Fragment>
+  );
 }
 
-export default Navbar
+export default Navbar;
