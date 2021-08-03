4d43efaba5c8efde08e11dc6cee57678e7cb46c8
emranarsala <73989490+emranarsala@users.noreply.github.com>
2021-08-03 14:05:56 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-// import React from 'react';
-// import logo from './logo.svg';
-import React, { Component }  from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// import components
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component }  from 'react';
 import Home from './components/Pages/Home';
-import AddCompany from "./components/add-company.component";
-import Company from "./components/company.component";
-import CompaniesList from "./components/companies-list.component";
+import StockData from './components/Pages/StockData';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route exact path={["/", "/companies"]} component={CompaniesList} />
-          <Route exact path="/add" component={AddCompany} />
-          <Route path="/companies/:id" component={Company} />
-        </Switch>
-        </Router>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/stockdata' exact component={StockData}/>
+      </Switch>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
